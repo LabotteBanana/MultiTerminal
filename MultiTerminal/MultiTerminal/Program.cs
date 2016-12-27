a70615a17e9a74009db10e5e39b06565a866725a
김진수 <jinsu0286@daum.net>
2016-12-27 15:23:13 +0900

    주석빼고 커밋

--- MultiTerminal/MultiTerminal/Program.cs
+++ MultiTerminal/MultiTerminal/Program.cs
@@ -17,7 +17,6 @@ namespace MultiTerminal
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-            ////aaaaaa주석으로 커밋테스트
         }
     }
 }
