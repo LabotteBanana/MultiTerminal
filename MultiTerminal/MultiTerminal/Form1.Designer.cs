76c6fefd2708f0c211e92c7c678a5010f8d87f1f
LabotteBanana <chs93114@gmail.com>
2016-12-27 17:12:21 +0900

    으라차아

--- MultiTerminal/MultiTerminal/Form1.Designer.cs
+++ MultiTerminal/MultiTerminal/Form1.Designer.cs
@@ -29,12 +29,57 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.T1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // T1
+            // 
+            this.T1.Location = new System.Drawing.Point(56, 64);
+            this.T1.Name = "T1";
+            this.T1.Size = new System.Drawing.Size(100, 21);
+            this.T1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(56, 103);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(54, 143);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(553, 476);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.T1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox T1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- MultiTerminal/MultiTerminal/Form1.cs
+++ MultiTerminal/MultiTerminal/Form1.cs
@@ -8,13 +8,28 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+// 블루투스 
+using System.IO.Ports;
+
+
 namespace MultiTerminal
 {
     public partial class Form1 : Form
     {
+        private SerialPort arduSerialPort = new SerialPort();
         public Form1()
         {
             InitializeComponent();
+            arduSerialPort.PortName = "COM15";    //아두이노가 연결된 시리얼 포트 번호 지정
+            arduSerialPort.BaudRate = 9600;       //시리얼 통신 속도 지정
+            arduSerialPort.Open();                //포트 오픈
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string textbox1;
+            textbox1 = T1.Text;
+            arduSerialPort.Write(textbox1);
         }
     }
 }
