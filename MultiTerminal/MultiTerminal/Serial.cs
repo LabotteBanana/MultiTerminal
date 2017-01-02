ae86995e1d975ddff0908dd82e43e0b432ef5a22
LabotteBanana <chs93114@gmail.com>
2017-01-03 00:32:22 +0900

    진수형 자료 + 유아트 연결 추가 커밋

---
+++ MultiTerminal/MultiTerminal/Serial.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+
+namespace MultiTerminal
+{
+    class Serial
+    {
+        SerialPort sPort;
+
+
+    }
+}
