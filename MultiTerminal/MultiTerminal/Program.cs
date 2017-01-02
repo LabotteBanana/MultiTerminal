06d0085d4ab151a0c826011110ceb9c33c0be5d7
LabotteBanana <chs93114@gmail.com>
2017-01-02 22:29:05 +0900

    f

--- MultiTerminal/MultiTerminal/Program.cs
+++ MultiTerminal/MultiTerminal/Program.cs
@@ -21,8 +21,6 @@ namespace MultiTerminal
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-
-            
         }
     }
 }
---
+++ MultiTerminal/MultiTerminal/Server.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// 블루투스 
+using System.IO.Ports;
+
+
+namespace MultiTerminal
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
