6c198ecd34750d81f7b3c43f625e9268c5689316
LabotteBanana <chs93114@gmail.com>
2017-01-02 22:29:20 +0900

    Revert "dd"
    This reverts commit 0906c8c012291bcf360719d036c7a047f93da700.

--- MultiTerminal/MultiTerminal/Form1.Designer.cs
+++ MultiTerminal/MultiTerminal/Form1.Designer.cs
@@ -29,9 +29,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
