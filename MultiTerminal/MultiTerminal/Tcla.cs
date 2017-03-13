5576593b340f0591e5126198d3bf4d5aadda5690
MyHome <MyHome@DESKTOP-8SBESRU>
2017-03-13 17:55:18 +0900

    tcp초반

--- MultiTerminal/MultiTerminal/Tcla.cs
+++ MultiTerminal/MultiTerminal/Tcla.cs
@@ -6,13 +6,18 @@ using System.Threading.Tasks;
 using System.Net.Sockets;
 using System.Net;
 using System.Net.NetworkInformation;
-
+using System.IO;
 namespace MultiTerminal
 {
     class Tcla
     {
         private Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        private NetworkStream ns;
+        private StreamReader sr;
+        private StreamWriter sw;
+
         public NetworkInterface[] adapters = NetworkInterface.GetAllNetworkInterfaces();
+        
         private byte[] data = new byte[1024];
         public void DisplayNetworkInfo()
         {
@@ -66,6 +71,11 @@ namespace MultiTerminal
             {
                 IPAddress host = IPAddress.Parse(ip);
                 IPEndPoint ipep = new IPEndPoint(host, port);
+                ns = new NetworkStream(sock);
+
+                sr = new StreamReader(ns);
+                sw = new StreamWriter(ns);
+
                 sock.Connect(ipep);
                 sock.Send(Encoding.ASCII.GetBytes("Hello Server!"));
                 sock.Receive(data);
@@ -89,13 +99,65 @@ namespace MultiTerminal
             sock.Close();
             return true;
         }
-        public string SendMsg()
+        public bool SendMsg(byte[] data)
         {
-            return "Send";
+            try
+            {
+                int total = 0;
+                int size = data.Length;
+                int left_data = size;
+                int send_data = 0;
+
+                // 전송할 데이터의 크기 전달
+                byte[] data_size = new byte[4];
+                data_size = BitConverter.GetBytes(size);
+                send_data = sock.Send(data_size);
+
+                //실제 데이터 전송
+                while(total <size )
+                {
+                    send_data = sock.Send(data, total, left_data, SocketFlags.None);
+                    total += send_data;
+                    left_data -= send_data;
+                }
+                return true;
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
         }
-        public string RecvMsg()
+        public byte[] RecvMsg()
         {
-            return "Recv";
+            try
+            {
+                int total = 0;
+                int size = 0;
+                int left_data = 0;
+                int recv_data = 0;
+
+                //수신할 데이터 크기 알아내기
+                byte[] data_size = new byte[4];
+                recv_data = sock.Receive(data_size, 0, 4, SocketFlags.None);
+                size = BitConverter.ToInt32(data_size, 0);
+                left_data = size;
+
+                byte[] data = new byte[size];
+                while(total<size)
+                {
+                    recv_data = sock.Receive(data, total, left_data, 0);
+                    if (recv_data == 0) break;
+                    total += recv_data;
+                    left_data -= recv_data;
+                }
+                return data;
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
 
         }
         public void GetState()
--- MultiTerminal/MultiTerminal/Tserv.cs
+++ MultiTerminal/MultiTerminal/Tserv.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using System.Net.Sockets;
 using System.Net;
 using System.Net.NetworkInformation;
+using System.IO;
+
 namespace MultiTerminal
 {
     class Tserv
@@ -13,6 +15,9 @@ namespace MultiTerminal
         private Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
         public NetworkInterface[] adapters = NetworkInterface.GetAllNetworkInterfaces();
         private byte[] data = new byte[1024];
+        private NetworkStream ns;
+        private StreamReader sr;
+        private StreamWriter sw;
 
         public void DisplayNetworkInfo()
         {
@@ -71,6 +76,11 @@ namespace MultiTerminal
                 Socket client = sock.Accept();
                 IPEndPoint claIP = (IPEndPoint)client.RemoteEndPoint;
 
+                ns = new NetworkStream(sock);
+                sr = new StreamReader(ns);
+                sw = new StreamWriter(ns);
+
+
                 //Console.WriteLine("{0}주소,{1}포트 접속", claIP.Address, claIP.Port);
 
                 sock.Send(Encoding.ASCII.GetBytes("Hello Client!"));
@@ -102,13 +112,66 @@ namespace MultiTerminal
             sock.Close();
             return true;
         }
-        public string SendMsg()
+        public bool SendMsg(byte[] data)
         {
-            return "Hello";
+            try
+            {
+                int total = 0;
+                int size = data.Length;
+                int left_data = size;
+                int send_data = 0;
+
+                // 전송할 데이터의 크기 전달
+                byte[] data_size = new byte[4];
+                data_size = BitConverter.GetBytes(size);
+                send_data = sock.Send(data_size);
+
+                //실제 데이터 전송
+                while (total < size)
+                {
+                    send_data = sock.Send(data, total, left_data, SocketFlags.None);
+                    total += send_data;
+                    left_data -= send_data;
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
         }
-        public string RecvMsg()
+        public byte[] RecvMsg()
         {
-            return "Bye";
+            try
+            {
+                int total = 0;
+                int size = 0;
+                int left_data = 0;
+                int recv_data = 0;
+
+                //수신할 데이터 크기 알아내기
+                byte[] data_size = new byte[4];
+                recv_data = sock.Receive(data_size, 0, 4, SocketFlags.None);
+                size = BitConverter.ToInt32(data_size, 0);
+                left_data = size;
+
+                byte[] data = new byte[size];
+                while (total < size)
+                {
+                    recv_data = sock.Receive(data, total, left_data, 0);
+                    if (recv_data == 0) break;
+                    total += recv_data;
+                    left_data -= recv_data;
+                }
+                return data;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+
         }
         public void GetState()
         {
